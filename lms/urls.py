3be93713b5999af06b9189e81cd6523d419d1b32
Rafael-Mota-Moraes-Moreira <rafaelmota622@gmail.com>
2025-04-13 19:56:47 -0300

    Removido criação de cursos

--- lms/urls.py
+++ lms/urls.py
@@ -4,6 +4,4 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home-page'),
     path('meus-cursos/', views.my_courses, name='my-courses'),
-    path('cadastrar-curso/', views.create_course, name='create-course'),
-    path('editar-curso/', views.create_course, name='edit-course'),
 ]
